efab8f07d66c13a0915a950c2bbe0a6858bd32db
eeevvv <evan.w.and@gmail.com>
2021-11-28 15:21:56 -0500

    FEAT: take CLI arguments and print them to screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use structopt::StructOpt;
+
+/// Start or stop a pomodoro instance.
+#[derive(StructOpt)]
+struct Cli {
+    /// start or stop
+    command: String,
+    /// work or break
+    unit: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+
+    let command_message = match args.command.as_str() {
+        "start" => "starting timer",
+        "stop" => "stopping timer",
+        _ => "try running pom again with 'start' or 'stop' as the first arguments"
+    };
+
+    let unit_message = match args.unit.as_str() {
+        "work" => "work",
+        "break" => "break",
+        _ => "second argument must be either 'work' or 'break'"
+    };
+
+    println!("{}\n{}", command_message, unit_message);
 }
